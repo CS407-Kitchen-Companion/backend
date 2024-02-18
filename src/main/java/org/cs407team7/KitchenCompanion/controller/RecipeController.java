ee7ba0415b85906ebd56fcc6a6dc1a3c6e3ffc3e
Javad228 <jbaghiro@purdue.edu>
2024-02-18 15:01:57 -0500

    recipes can now be viewed

--- src/main/java/org/cs407team7/KitchenCompanion/controller/RecipeController.java
+++ src/main/java/org/cs407team7/KitchenCompanion/controller/RecipeController.java
@@ -6,10 +6,7 @@ import org.cs407team7.KitchenCompanion.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -42,4 +39,20 @@ public class RecipeController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(500, "Internal Server Error"));
         }
     }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Object> getRecipe(@PathVariable Long id) {
+        try {
+            Recipe recipe = recipeService.getRecipeById(id);
+            if (recipe != null) {
+                return ResponseEntity.ok(recipe);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse(500, "Internal Server Error"));
+        }
+    }
+
 }
